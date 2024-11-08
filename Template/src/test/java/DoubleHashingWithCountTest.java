acfa8e3e30812b24c3a6c4a5dda1164385a43267
P Cisneros <psy@psysviewfinder.com>
2024-11-07 23:41:44 -0800

    Add files via upload

---
+++ Template/src/test/java/DoubleHashingWithCountTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoubleHashingWithCountTest {
+
+    @org.junit.jupiter.api.Test
+    void primaryHash() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void secondaryHash() {
+    }
+}
---
+++ Template/src/test/java/HashTableTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void resize() {
+    }
+
+    @Test
+    void getKeyIterator() {
+    }
+
+    @Test
+    void getValueIterator() {
+    }
+}
